c3203555c5b6aa203aa1cedff2e4c06381ed4ffd
Knut Waale <kwaale@github.com>
2021-09-06 19:59:22 -0300

    Haciendo una prueba eliminando algo del connect

--- db.js
+++ db.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const { HOST, DB_NAME } = process.env;
 const MONGODB_URI = `mongodb://${HOST}/${DB_NAME}`;
 
-mongoose.connect(MONGODB_URI,{
-    useUnifiedTopology:true,
-    useNewUrlParser:true
-})
+mongoose.connect(MONGODB_URI,
+//     {
+//     useUnifiedTopology:true,
+//     useNewUrlParser:true
+// }
+)
 .then(()=> console.log(`DB ${DB_NAME} conectada`))
 .catch((error)=>console.error(error));
 
