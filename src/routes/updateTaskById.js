72998792dfd915e2e424407960e0a0d995228e15
Knut Waale <kwaale@github.com>
2021-09-06 19:57:55 -0300

    Update Terminada

--- src/routes/updateTaskById.js
+++ src/routes/updateTaskById.js
@@ -1,38 +1,60 @@
-const { findOneAndUpdate, findById } = require('../models/Task');
 const Task = require('../models/Task');
-
-
+const mongoose = require('mongoose');
 
 //router.put("/update/:id", updateTaskById);
+
+const valid = mongoose.Types.ObjectId.isValid;
+const body = {
+    title: "Optional Type: String",
+    description: "Optional Type: String",
+    status: "Optional Type: Boolean"
+}
+
+const request = 'PUT:  /update/:id'; 
+
 const updateTaskById = async (req, res) => {
     try {
         const { id } = req.params;
-        if(!id) return res.json({
-            message:'ID is required'
-        })
+
+        if (!id) return res.json({
+            message: 'ID is required',
+            request,
+            body
+        });
+
+        if(!valid(id)) return res.json({
+            message: 'ID is invallid to DB',
+            request,
+            body
+        });
+
         const {
             title,
             description,
             status
         } = req.body;
-        if(title || description || status){
-            // const task = await Task.findById(id);
-            if(!task) return res.json(
-                {message:'Debe ser un ID existente'}
-                )
-                const task = await Task.findOneAndUpdate({ _id: id }, { title, description, status });
-                console.log('task',task)
-                console.log('task')
-                return res.json({
-                    message:'Tarea actualizada'
-                })
-        }else return res.json({
-            message:'Debe recibir un title, description o status para modicar'
-        });
-        
-        
-    } catch (error) {
 
+        if (title || description || status) {
+            const task = await Task.findOneAndUpdate({ _id: id }, { title, description, status }) || undefined;            
+            if (!task) res.json({
+                        message: 'Debe ser un ID existente',
+                        request,
+                        body                        
+                });
+            return res.json({
+                message: 'Task updated',
+                request,
+                body
+            });
+        } else {
+            return res.json({
+                message: 'You must receive a title, description or status to modify',
+                request,
+                body
+            });
+        }
+    } catch (error) {
+        console.error('******',error)
     }
 }
 
