a7ed6f2527979f2de50b294c3fe4d849f5800bf4
Knut Waale <kwaale@github.com>
2021-09-06 20:00:12 -0300

    Agregando ruta pruebas ELIMINAR ESTA RUTA

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const getTasks = require('./getTasks');
 const addTask = require('./addTask');
 const deleteTaskById = require('./deleteTaskById');
 const updateTaskById = require('./updateTaskById');
+const pruebas = require('./pruebas');
 
-//Metodo para ingresar rutas deleteTask
+//Metodo para ingresar rutas
 const router = express.Router();
 
 router.get("/", getTasks);
+router.get("/pruebas/:id", pruebas);
 router.post("/add", addTask);
 router.post("/delete", deleteTaskById);
 router.put("/update/:id", updateTaskById);
