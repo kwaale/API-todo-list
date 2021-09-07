c50e5142fa50549e69fdc41f0d96b031306ae429
Knut Waale <kwaale@github.com>
2021-09-07 12:00:17 -0300

    Delete terminada

--- src/routes/deleteTaskById.js
+++ src/routes/deleteTaskById.js
@@ -1,9 +1,32 @@
 const Task = require('../models/Task');
+const mongoose = require('mongoose');
+
+const validateId = mongoose.Types.ObjectId.isValid;
+
+const request = 'DELETE:  /delete/:id';
+
 const deleteTaskById = async (req, res) =>{
     try {
-        console.log('deleteTaskById')
+        const { id } = req.params;
+
+        if(!validateId(id)) res.json({
+            message: 'ID is invalid',
+            request
+        });
+
+        const task = await Task.findByIdAndDelete(id);
+
+        if(!task) res.json({
+            message: 'ID does not exist',
+            request
+        })
+
+        return res.json({
+            message: 'Task deleted',
+            task
+        })
     } catch (error) {
-        
+        console.error(error);
     }
 }
 
