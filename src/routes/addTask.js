6fb2561576c6d34cdefac1035f9af047ae8b4988
Knut Waale <kwaale@github.com>
2021-09-07 12:12:33 -0300

    add Task terminada

--- src/routes/addTask.js
+++ src/routes/addTask.js
@@ -1,12 +1,26 @@
 const Task = require('../models/Task');
+
+const body = {
+    title: "Required Type: String",
+    description: "Optional Type: String",
+}
+
+const request = 'POST:  /add'; 
+
 const addTask = async (req, res) =>{
     try {
         const { title, description } = req.body;
+        if(!title) res.json({
+            message: 'Title is required',
+            request,
+            body
+        });
+
         const task = await new Task({title,description});
         await task.save();
         return res.json(task)
     } catch (error) {
-        
+        console.error(error);
     }
 }
 
